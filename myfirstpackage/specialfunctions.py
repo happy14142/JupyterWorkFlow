1b05c1ccbf7a927ed95c475d8119742adb754932
Andrea Allegra <aallegra@numerix.com>
2017-04-01 12:22:42 +0100

    added my first package

---
+++ myfirstpackage/specialfunctions.py
@@ -0,0 +1,19 @@
+def specialsum(a,b):
+
+    """ make the sum of two variables
+
+    Parameters
+    -----------------
+    a = double(required)
+	first value to be summed
+    b = double(required)
+	second value to be summed
+
+    Returns
+    ---------------
+    sum of two numbers
+   
+    """
+	
+    results = a + b
+    return results
